c00d380f16c4bb0c9d8350b908874f5aa64217fb
jiyuujin <jiyuujinunite@gmail.com>
2021-02-26 19:50:20 +0900

    fix: update for MainFormat

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import TopPageContainer from './containers/TopPageContainer'
-import logo from './assets/logo.svg'
 
 const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <TopPageContainer />
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <TopPageContainer />
+        </>
     )
 }
 
--- src/components/MainFormat.tsx
+++ src/components/MainFormat.tsx
@@ -1,5 +1,6 @@
 import { NekoInput, NekoButton } from 'nekohack-ui'
 import { TopPageHandler } from '../containers/TopPageContainer'
+import logo from '../assets/logo.svg'
 
 interface HelloWorldProps {
     inputValue: string
@@ -10,19 +11,35 @@ type Props = HelloWorldProps & TopPageHandler
 
 const MainFormat = (props: Props) => {
     return (
-        <section>
-            <div>
-                <NekoInput
-                    value={props.inputValue}
-                    onChange={props.handleOnChangeValue}
-                />
-            </div>
-            <div>
-                <NekoButton onClick={() => props.handleOnClick()}>
-                    count is: {props.clickCount}
-                </NekoButton>
-            </div>
-        </section>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <section>
+                    <div>
+                        <NekoInput
+                            value={props.inputValue}
+                            onChange={props.handleOnChangeValue}
+                        />
+                    </div>
+                    <div>
+                        <NekoButton onClick={() => props.handleOnClick()}>
+                            count is: {props.clickCount}
+                        </NekoButton>
+                    </div>
+                </section>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
     )
 }
 
